896fc0b159cf5147b2a56d4b44eb7627149c065d
madoron <nasu2nasu@gmail.com>
2016-08-17 17:28:16 +0900

    シーン分割作業中

---
+++ PersianNight/scene/Scene_OASIS.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+using GameFramework;
+using DxLibDLL;
+
+namespace PersianNight
+{
+	//================================================================================
+	//	
+	//================================================================================
+	public class Scene_OASIS : Scene
+	{
+	}
+}
